34aa6a2f13633bd448b7a2c24298e808cdd589cd
CleverSource <stunthound@gmail.com>
2025-10-21 16:02:26 -0400

    Fix Fish setup

--- setup.fish
+++ setup.fish
@@ -1,6 +1,12 @@
 #!/usr/bin/env fish
 
-chsh -s (which fish) (whoami)
+if set -q SUDO_USER; and test "$SUDO_USER" != "root"
+    set REAL_USER $SUDO_USER
+else
+    set REAL_USER (whoami)
+end
+
+chsh -s (which fish) $REAL_USER
 
 # Install fisher and fzf plugin
 if not functions -q fisher
