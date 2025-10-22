28d0419b4b189b99ef26887641044e37d4d8bb92
CleverSource <stunthound@gmail.com>
2025-10-22 04:37:37 -0400

    Fix fish setup

--- setup.fish
+++ setup.fish
@@ -1,17 +1,11 @@
 #!/usr/bin/env fish
 
-if set -q SUDO_USER; and test "$SUDO_USER" != "root"
-    set REAL_USER $SUDO_USER
-else
-    set REAL_USER (whoami)
-end
-
-chsh -s (which fish) $REAL_USER
+chsh -s (which fish)
 
 # Install fisher and fzf plugin
 if not functions -q fisher
     echo "Installing fisher..."
-    pacman -S --noconfirm fisher
+    sudo pacman -S --noconfirm fisher
 end
 
 fisher install PatrickF1/fzf.fish
